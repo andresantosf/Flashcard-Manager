5b05c2b8d79e7da4b746f8f77cb28a3fae4aa164
andresantosf <santos.andref17@gmail.com>
2026-07-10 22:37:17 -0300

    app.tsx

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <view style={styles.container}>
+      <text>Flashcard Manager</text>
+      <statusbar style="auto" />
+    </view>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
